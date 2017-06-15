fef499d9cc8542aa32f8060a1909c80ccc3a8a73
yolanda philgreen <yolanda.philgreen@gmail.com>
2017-06-15 15:05:52 -0600

    arrays squares project

---
+++ src/edu/cnm/deepdive/arrays/Squares.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.arrays;
+
+import java.util.Arrays;
+
+public class Squares {
+
+  public static void main(String[] args) {
+  int [] squares = new int [50];
+  for (int i = 0; i < squares.length; i++) {
+    squares [i] = i*i;
+    
+    
+  } // end for
+  System.out.println(Arrays.toString(squares));
+  } // end array string
+
+} // end class
