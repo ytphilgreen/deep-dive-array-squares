c62dc2e5794f01a9626657bff6b5accc3e0a0d3c
yolanda philgreen <yolanda.philgreen@gmail.com>
2017-06-15 15:08:15 -0600

    formatted project

--- src/edu/cnm/deepdive/arrays/Squares.java
+++ src/edu/cnm/deepdive/arrays/Squares.java
@@ -5,13 +5,11 @@ import java.util.Arrays;
 public class Squares {
 
   public static void main(String[] args) {
-  int [] squares = new int [50];
-  for (int i = 0; i < squares.length; i++) {
-    squares [i] = i*i;
-    
-    
-  } // end for
-  System.out.println(Arrays.toString(squares));
+    int[] squares = new int[50];
+    for (int i = 0; i < squares.length; i++) {
+      squares[i] = i * i;
+    } // end for
+    System.out.println(Arrays.toString(squares));
   } // end array string
 
 } // end class
